57d6bad45e13cf7e2a3fbc4e379a436b154a5a1a
KrzySto <kskrzysiek788@gmail.com>
2021-02-01 18:30:13 +0100

    Nowa funkcjonalość - Policz

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 
-//int Policz (int a, int b)
-//{
-//    return a+b;
-//}
+int Policz (int a, int b)
+{
+    return a+b;
+}
 
 int main() {
     std::cout << "Hello world" << std::endl;
-//    std::cout << "suma: " << Policz(2,3) << std::endl;
+    std:: cout <<"W pierwszym branchu; 2+3=" << Policz(2,3) <<std::endl;
     return 0;
 }
