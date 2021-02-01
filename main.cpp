b1df1508e204a975eaeebb1b34182da926a4afa6
KrzySto <kskrzysiek788@gmail.com>
2021-02-01 18:31:43 +0100

    usuniete komentarze

--- main.cpp
+++ main.cpp
@@ -1,12 +1,7 @@
 #include <iostream>
 
-//int Policz (int a, int b)
-//{
-//    return a+b;
-//}
 
 int main() {
     std::cout << "Hello world" << std::endl;
-//    std::cout << "suma: " << Policz(2,3) << std::endl;
     return 0;
 }
